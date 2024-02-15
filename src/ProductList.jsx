8e19cc23462d755b1360fe359d0e2313e5a78984
AZTechDevelop <157212350+AZTechDevelop@users.noreply.github.com>
2024-02-15 07:46:53 +0000

    :)

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,10 +4,10 @@ import ProductItem from './ProductItem';
 
 function ProductList({ onSelect }) {
   const produse = [
-    { id: 1, nume: 'Ceaiuri'},
-    { id: 2, nume: 'Tincturi'},
-    { id:3, nume:'Alifii'},
-    { id:4, nume:'Siropuri'}
+    { id: 1, nume: 'tip a'},
+    { id: 2, nume: 'tip b'},
+    { id:3, nume:'tip c'},
+    { id:4, nume:'tip d'}
   ];
 
   return (
